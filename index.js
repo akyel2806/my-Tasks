f976ed3e29af479c71dbc51935d7ee10cb762217
Nama Anda <email@example.com>
2025-10-15 07:22:36 +0700

    UI My-tasks

--- index.js
+++ index.js
@@ -33,31 +33,31 @@ app.post('/api/register', async (c) => {
 });
 
 app.post('/api/login', async (c) => {
-  const { username, password } = await c.req.json();
-  const user = await db.query.users.findFirst({ where: (users, { eq }) => eq(users.username, username ) });
-
-  if (!user) return c.json({ success: false, message: 'Username atau password salah' }, 401);
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) return c.json({ success: false, message: 'Username atau Password salah' }, 401);
-
-  const token = jwt.sign({ id: user.id, username: user.username}, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-  setCookie(c, 'token', token, { httpOnly: true, sameSite: 'Lax', maxAge: 3600 });
-
-  return c.json({ success: true, message: 'Login Berhasil' });
+    const { username, password } = await c.req.json();
+    const user = await db.query.users.findFirst({ where: (users, { eq }) => eq(users.username, username) });
+ 
+    if (!user) return c.json({ success: false, message: 'Username atau password salah' }, 401);
+    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return c.json({ success: false, message: 'Username atau password salah' }, 401);
+ 
+    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+    setCookie(c, 'token', token, { httpOnly: true, sameSite: 'Lax', maxAge: 3600 });
+    
+    return c.json({ success: true, message: 'Login berhasil' });
 });
 
 app.get('/api/me', (c) => {
-  const token = getCookie(c, 'token');
-  if (!token) return c,json({ success: false, message: 'Unauthorized' }, 401);
-  try {
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    
+const token = getCookie(c, 'token');
+    if (!token) return c.json({ success: false, message: 'Unauthorized' }, 401);
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        
     return c.json({ success: true, data: user });
-  } catch (error) {
-    return c.json({ success: false, message: 'Unauthorized' }, 401);
-  }
+    } catch (error) {
+        return c.json({ success: false, message: 'Unauthorized' }, 401);
+    }
 });
 
 app.post('/logout', (c) => {
@@ -99,7 +99,7 @@ if (process.env.VERCEL) {
   console.log('Running on Vercel');
   globalThis.app = app;
 } else {
-  const port = 3000;
+  const port = 5000;
   console.log(`✅ Server is running on http://localhost:${port}`);
   serve({
     fetch: app.fetch,
